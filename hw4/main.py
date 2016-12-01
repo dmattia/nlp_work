35bf9eadae14367e9d2a17b1ede689f4f978be90
dmattia <dmattia@nd.edu>
2016-12-01 07:42:32 -0500

    Finished cky parsing algorithm non-weighted

--- hw4/main.py
+++ hw4/main.py
@@ -20,6 +20,47 @@ class CFG(object):
   def rules(self):
     return itertools.chain.from_iterable(self._rules.values())
 
+  def cky(self, string):
+    """ Finds the highest probability parse of a given string
+    """
+    # Create Chart
+    words = string.strip().split(" ")
+    n = len(words)
+    chart = [[set() for i in range(n)] for i in range(n)]
+
+    # Do top row
+    for i in range(1, n+1):
+      word = words[i-1]
+      for rule in self.rules:
+        if word in rule.goes_to:
+          #print(word + " found in " + str(rule))
+          chart[i-2][i-1].add(rule.base)
+      if len(chart[i-2][i-1]) == 0:
+        # This word is unk
+        word = "<unk>"
+        for rule in self.rules:
+         if word in rule.goes_to:
+           #print(word + " found in " + str(rule))
+           chart[i-2][i-1].add(rule.base)
+
+    # Fill in other rows
+    for l in range(2, n + 1):
+      for i in range(0, n-l+1):
+        j = i + l
+        for k in range(i+1, j):
+          #print("Checking Y in " + str(chart[i-1][k-1]) + " and Z in " + str(chart[k-1][j-1]))
+          binary_rules = filter(lambda x: len(x.goes_to) == 2, self.rules)
+          for rule in binary_rules:
+            Y = rule.goes_to[0]
+            Z = rule.goes_to[1]
+            #print(str(rule) + " :::::::::: " + Y + " " + Z)
+            if Y in chart[i-1][k-1] and Z in chart[k-1][j-1]:
+              #print(rule)
+              chart[i-1][j-1].add(rule.base)
+
+    #print("CKY RESULT: " + str(chart[n-1][n-1]))
+    return chart[n-1][n-1] is None
+
   def conditional_probability(self, rule):
     rulelist = self._rules[rule.base]
     total = sum(rule.times_seen  for rule in rulelist)
@@ -49,6 +90,12 @@ class Rule(object):
 
   def __str__(self):
     return self.base + " -> " + " ".join(self.goes_to) + " # " + str(self.times_seen)
+
+  def __hash__(self):
+    result = hash(self.base)
+    for token in self.goes_to:
+      result ^= hash(token)
+    return result
 
   def __eq__(self, other):
     return self.base == other.base and self.goes_to == other.goes_to
@@ -81,3 +128,11 @@ if __name__ == "__main__":
   rules.sort(key = lambda x: cfg.conditional_probability(x), reverse=True)
   for rule in rules[:5]:
     print(str(rule).split("#")[0] + "# " + str(cfg.conditional_probability(rule)))
+
+  print("\nCKY Parse")
+  #cfg.cky("Which ones stop in Nashville ?")
+  #cfg.cky("Which is last ?")
+  with open("dev.strings") as devFile:
+    lines = devFile.readlines()
+    for line in lines:
+      print(cfg.cky(line))
