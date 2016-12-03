5c47df9e07208e5607f7bae97b873b39d2cb897e
dmattia <dmattia@nd.edu>
2016-12-03 16:42:01 -0500

    Added rules as objects to cky algorithm

--- hw4/main.py
+++ hw4/main.py
@@ -6,6 +6,7 @@ import itertools
 class CFG(object):
   def __init__(self):
     self._rules = defaultdict(list)
+    self.words_seen = set()
 
   def add_rule(self, rule):
     if rule not in self._rules[rule.base]:
@@ -20,28 +21,40 @@ class CFG(object):
   def rules(self):
     return itertools.chain.from_iterable(self._rules.values())
 
+  @property
+  def bases(self):
+    return set([rule.base for rule in self.rules])
+
+  def train(self, filename):
+    with open(filename) as treeFile:
+      lines = treeFile.readlines()
+      for line in lines:
+        tree = Tree.from_str(line)
+        for node in tree.bottomup():
+          if len(node.children) > 0:
+            rule = Rule(node.label, [child.label for child in node.children])
+            self.add_rule(rule)
+          else:
+            # Node is a word
+            self.words_seen.add(node.label)
+
   def cky(self, string):
     """ Finds the highest probability parse of a given string
     """
     # Create Chart
     words = string.strip().split(" ")
     n = len(words)
-    chart = [[set() for i in range(n)] for i in range(n)]
+    chart = [[set() for i in range(n+1)] for i in range(n+1)]
 
     # Do top row
     for i in range(1, n+1):
       word = words[i-1]
+      if word not in self.words_seen:
+        word = "<unk>"
       for rule in self.rules:
         if word in rule.goes_to:
           #print(word + " found in " + str(rule))
-          chart[i-2][i-1].add(rule.base)
-      if len(chart[i-2][i-1]) == 0:
-        # This word is unk
-        word = "<unk>"
-        for rule in self.rules:
-         if word in rule.goes_to:
-           #print(word + " found in " + str(rule))
-           chart[i-2][i-1].add(rule.base)
+          chart[i-1][i].add(rule)
 
     # Fill in other rows
     for l in range(2, n + 1):
@@ -54,12 +67,23 @@ class CFG(object):
             Y = rule.goes_to[0]
             Z = rule.goes_to[1]
             #print(str(rule) + " :::::::::: " + Y + " " + Z)
-            if Y in chart[i-1][k-1] and Z in chart[k-1][j-1]:
-              #print(rule)
-              chart[i-1][j-1].add(rule.base)
+            y_set = map(lambda rule: rule.base, chart[i][k])
+            z_set = map(lambda rule: rule.base, chart[k][j])
+            if Y in y_set and Z in z_set:
+              chart[i][j].add(rule)
 
-    #print("CKY RESULT: " + str(chart[n-1][n-1]))
-    return chart[n-1][n-1] is None
+    """
+    # print chart
+    for i in range(n+1):
+      for j in range(n+1):
+        print(str(i) + ":" + str(j))
+        for rule in chart[i][j]:
+          print("\t" + str(rule))
+    """
+
+    bases = [rule.base for rule in chart[0][n]]
+    print(len(filter(lambda base: base == "TOP", bases)))
+    return 'TOP' in bases
 
   def conditional_probability(self, rule):
     rulelist = self._rules[rule.base]
@@ -103,15 +127,7 @@ class Rule(object):
 if __name__ == "__main__":
   cfg = CFG()
 
-  with open("train.trees.pre.unk") as treeFile:
-    lines = treeFile.readlines()
-    for line in lines:
-      tree = Tree.from_str(line)
-      for node in tree.bottomup():
-        if len(node.children) > 0:
-          rule = Rule(node.label, [child.label for child in node.children])
-          cfg.add_rule(rule)
-
+  cfg.train("train.trees.pre.unk")
   #print cfg
   print("Length: " + str(len(cfg)))
 
@@ -134,5 +150,8 @@ if __name__ == "__main__":
   #cfg.cky("Which is last ?")
   with open("dev.strings") as devFile:
     lines = devFile.readlines()
+    #lines = devFile.readlines()[37:38]
     for line in lines:
-      print(cfg.cky(line))
+      parse = cfg.cky(line)
+      if not parse:
+        print(line.strip())
